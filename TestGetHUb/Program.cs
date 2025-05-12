1affaed5cfa64e58f2318e19756512dedcc23f50
ImanAdam331 <imanadamzedjali@gmail.com>
2025-05-12 15:02:43 +0400

    Add project files.

---
+++ TestGetHUb/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestGetHUb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!2 ");
+
+        }
+    }
+}
